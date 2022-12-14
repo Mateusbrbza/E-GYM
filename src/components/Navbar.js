636b709079102c0e36dd131394c04cce70f48784
Mateusbrbza <mateus.c.barboza@gmail.com>
2022-12-13 22:23:59 -0300

    starting navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Stack } from '@mui/material';
 
 const Navbar = () => {
   return (
@@ -6,4 +8,4 @@ const Navbar = () => {
   )
 }
 
-export default Navbar
+export default Navbar;
