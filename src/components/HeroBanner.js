471675ef538a9356c008a051ca17eb50641771be
Mateusbrbza <mateus.c.barboza@gmail.com>
2022-12-18 01:40:12 -0300

    montando os hooks

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Box, Stack, Typography, Button } from '@mui/material';
 
-import HeroBannerImage from '../assets/images/banner3.jpg';
+import HeroBannerImage from '../assets/images/banner4.png';
 
 const HeroBanner = () => {
   return (
--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -21,12 +21,37 @@ const SearchExercises = () => {
 
       <Box position="relative" mb="72px">
         <TextField 
+        sx={{
+          input: { 
+            fontWeight: '700', 
+            border: 'none', 
+            borderRadius: '5px'
+          },
+          width: { lg: '800px', xs: '350px'},
+          backgroundColor: '#FFF', 
+          borderRadius: '40px'
+        }}
         height="76px"
         value=""
         onChange={(e) => {}}
         placeholder="Pesquisar exercicios"
         type="text"
         />
+        <Button 
+        className="search-btn"
+        sx={{
+          bgcolor: '#FF2625',
+          color: '#fff',
+          textTransform: 'none',
+          width: {lg: '150px', xs: '80px'},
+          fontSize: {lg: '20px', xs: '14px'},
+          height: '56px',
+          position: 'absolute',
+          right: '0'
+        }}
+        >
+          Pesquisar
+        </Button>
       </Box>
     </Stack>
   )
