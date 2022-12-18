ea3f2de9bbfb43076a2819a989ef478d20643aa9
Mateusbrbza <mateus.c.barboza@gmail.com>
2022-12-18 01:20:09 -0300

    comecando aba de pesquisa

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState} from 'react';
+import { 
+  Box, 
+  Button, 
+  Stack, 
+  TextField, 
+  Typography } from '@mui/material';
 
 const SearchExercises = () => {
   return (
-    <div>SearchExercises</div>
+    <Stack alignItems="center" mt="37px"
+    justifyContent="center" padding="20px"
+    >
+      <Typography 
+      fontWeight={700}
+      sx={{ fontSize: { lg: '44px', xs: '30px'}}}
+      mb="50px" textAlign="center"
+      >
+        Exericios separados por <br /> grupos musculares
+      </Typography>
+
+      <Box position="relative" mb="72px">
+        <TextField 
+        height="76px"
+        value=""
+        onChange={(e) => {}}
+        placeholder="Pesquisar exercicios"
+        type="text"
+        />
+      </Box>
+    </Stack>
   )
 }
 
-export default SearchExercises
+export default SearchExercises;
