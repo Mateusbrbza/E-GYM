9da184252ecd94dde32a97693a439f6ec5d111f0
Mateusbrbza <mateus.c.barboza@gmail.com>
2022-12-18 02:28:52 -0300

    montando as chamadas de api

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -6,15 +6,44 @@ import {
   TextField, 
   Typography } from '@mui/material';
 
-
+import { exerciseOptions, fetchData} from '../utils/fetchData';
 
 
 const SearchExercises = () => {
   const [search, setSearch] = useState('');
+  const [exercises, setExercises] = useState([]);
+  const [bodyParts, setBodyParts] = useState([]);
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const bodyPartsData = await fetchData(
+        'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
+        exerciseOptions
+        );
+
+        setBodyParts(['all', ...bodyPartsData]);
+    }
+    
+    fetchExercisesData();
+  }, [])
+  
 
   const handleSearch = async () => {
     if(search) {
-     //const exercisesData = await fetchData();
+     const exercisesData = await fetchData(
+      'https://exercisedb.p.rapidapi.com/exercises',
+      exerciseOptions);
+
+      //console.log(exercisesData);
+      const searchedExercises = exercisesData.filter(
+        (exercise) => exercise.name.toLowerCase().includes(search)
+        || exercise.target.toLowerCase().includes(search)
+        || exercise.equipment.toLowerCase().includes(search)
+        || exercise.bodypart.toLowerCase().includes(search)
+      );
+
+      setSearch('');
+      setExercises(searchedExercises);
     }
   }
 
