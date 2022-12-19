b9cd24213c2763d99d42a2e9d6cf2fb2d4eb4888
Mateusbrbza <mateus.c.barboza@gmail.com>
2022-12-18 23:26:34 -0300

    ExerciseCard done

--- src/components/ExerciseCard.js
+++ src/components/ExerciseCard.js
@@ -12,6 +12,43 @@ const ExerciseCard = ({exercise}) => {
         src={exercise.gifUrl} 
         alt={exercise.name} 
         loading="lazy" />
+        <Stack
+        direction="row"
+        >
+            <Button
+            sx={{ 
+                ml: '21px', 
+                color: '#fff', 
+                background: '#ff1919',
+                fontSize: '14px', 
+                borderRadius: '20px', 
+                textTransform: 'capitalize'
+            }} >
+                {exercise.bodyPart}
+            </Button>
+            <Button
+            sx={{ 
+                ml: '21px', 
+                color: '#fff', 
+                background: '#ff1919',
+                fontSize: '14px', 
+                borderRadius: '20px', 
+                textTransform: 'capitalize'
+            }} >
+                {exercise.target}
+            </Button>
+            <Typography
+            ml="21px"
+            color="#000"
+            fontWeight="bold"
+            mt="11px"
+            pb="10px"
+            textTransform="capitalize"
+            fontSize="14px"
+            >
+                {exercise.name}
+            </Typography>
+        </Stack>
     </Link>
   )
 }
