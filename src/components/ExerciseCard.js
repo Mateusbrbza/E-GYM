2a89a0517a494828310fea61174580fe1ddac1ad
Mateusbrbza <mateus.c.barboza@gmail.com>
2022-12-18 23:02:19 -0300

    ExerciseCard gif

--- src/components/ExerciseCard.js
+++ src/components/ExerciseCard.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button, Stack, Typography } from'@mui/material';
 
-const ExerciseCard = () => {
+const ExerciseCard = ({exercise}) => {
   return (
-    <div>ExerciseCard</div>
+    <Link
+    to ={`/exercise/${exercise.id}`}
+    className="exercise-card"
+    >
+        <img 
+        src={exercise.gifUrl} 
+        alt={exercise.name} 
+        loading="lazy" />
+    </Link>
   )
 }
 
