d49c0195beb173833bc842ba74c27fdb3077ce91
Mateusbrbza <mateus.c.barboza@gmail.com>
2022-12-20 00:18:45 -0300

    finalizando descricao exercicio, iniciando cards

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -9,7 +9,6 @@ import EquipmentImage from '../assets/icons/equipment.png';
 const Detail = ({ exerciseDetail }) => {
     const { bodyPart, gifUrl, name, target, equipment } = exerciseDetail;
 
-    console.log(gifUrl);
 
   return (
     <Stack
@@ -17,6 +16,23 @@ const Detail = ({ exerciseDetail }) => {
     sx={{flexDirection: { lg: 'row' }, p: '20px', alignItems: 'center'
     }}>
         <img src={gifUrl} alt={name} Loading="lazy" className="detail-image" />
+        <Stack
+        sx={{ gap: { lg: '35px', xs: '20px'}}}
+        >
+            <Typography variant="h3">
+                {name}
+            </Typography>
+            <Typography variant="h6">
+                Nenhum cidadão tem o direito de ser um amador 
+                em matéria de treinamento físico. Que desgraça é 
+                para o homem envelhecer sem nunca ver a beleza 
+                e a força do que o seu corpo é capaz. <br /> -Sócrates 
+                <br />
+                <br />
+                <b>{name}</b> é um dos melhores exercícios para o seu 
+                {` `} <b>{target}</b>.
+            </Typography>
+        </Stack>
     </Stack>
   )
 }
