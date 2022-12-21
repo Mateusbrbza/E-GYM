a92e7329b65d32a279f653529f0dddc9971450f7
Mateusbrbza <mateus.c.barboza@gmail.com>
2022-12-21 00:21:36 -0300

    .

--- src/components/SimilarExercises.js
+++ src/components/SimilarExercises.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Box, Stack, Typography } from '@mui/material';
 
-const SimilarExercises = () => {
+const SimilarExercises = ({targetMuscleExercises, equipmentExercises}) => {
   return (
-    <div>SimilarExercises</div>
+    <Box
+    sx={{ mt: {lg: '100px', xs: '0px'}}}
+    >
+      <Typography>Exercicios similares para o mesmo grupo muscular</Typography>
+    </Box>
   )
 }
 
