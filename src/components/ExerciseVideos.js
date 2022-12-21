f3724ba5d31f71e2be399d20841c4230ad5e02dc
Mateusbrbza <mateus.c.barboza@gmail.com>
2022-12-20 23:32:20 -0300

    utilizando youtubeAPI para puxar thumbnail

--- src/components/ExerciseVideos.js
+++ src/components/ExerciseVideos.js
@@ -1,13 +1,43 @@
 import React from 'react';
 import { Typography, Box, Stack } from '@mui/material';
 
-const ExerciseVideos = ({ ExerciseVideos, name}) => {
+const ExerciseVideos = ({ exerciseVideos, name}) => {
+  if(!exerciseVideos.lengh) return 'Carregando...';
+
   return (
     <Box
     sc={{ marginTop: { lg: '200px', xs: '20px'}}}
     p="20px"
     >
-
+      <Typography
+      variant="h3" mb="33px"
+      >
+        Veja videos do exercicio 
+        <span style={{ color: '#ff2625', textTransform: "capitalize"}}>{name}</span>
+      </Typography>
+      <Stack
+      justifyContent="flex-start"
+      flexWrap="wrap"
+      alignItems="center"
+      sx={{
+        flexDirection: { lg: 'row'}, 
+        gap: { lg: '110px', xs: '0'},
+      }}
+      >
+        {exerciseVideos?.slice(0, 2).map((item, index) => (
+          <a
+          key={index}
+          className="exercise-video"
+          href={`https://www.youtube.com/watch?v=${item.video.videoId}`}
+          target="_blank"
+          rel="noreferrer"
+          >
+            <img 
+            src={item.video.thumbnails[0].url} alt={item.video.title}
+            />
+          </a>
+        ))}
+      </Stack>
     </Box>
   )
 }
